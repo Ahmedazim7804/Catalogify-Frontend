0e084675c45d3046e4b9ba74308351962d8c14a7
coderkhushal <137866759+coderkhushal@users.noreply.github.com>
2024-03-06 09:04:25 +0530

    boilerplate

---
+++ lib/Router/router.dart
@@ -0,0 +1,27 @@
+import "package:go_router/go_router.dart";
+
+import "package:flutter/material.dart";
+import "package:inno_hack/root_scaffold.dart";
+import "package:inno_hack/screens/home_page.dart";
+import "package:inno_hack/screens/second_screen.dart";
+
+
+final GoRouter router = GoRouter(
+  initialLocation: '/',
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const RootScaffold(child: HomeScreen());
+      },
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'second',
+          builder: (BuildContext context, GoRouterState state) {
+            return const RootScaffold(child: SecondScreen());
+          },
+        ),
+      ],
+    ),
+  ],
+);
---
+++ lib/root_scaffold.dart
@@ -0,0 +1,23 @@
+import "package:flutter/material.dart";
+import "package:google_fonts/google_fonts.dart";
+
+class RootScaffold extends StatelessWidget {
+  final Widget child;
+
+  const RootScaffold({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        foregroundColor: Colors.white,
+        title: Center(
+          child: Text("Innohacks", style: GoogleFonts.adamina()),
+        ),
+      ),
+      body: child,
+    ));
+  }
+}
