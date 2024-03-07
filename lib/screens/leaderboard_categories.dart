e7132b451a6883db897a1067079cc9763ef0abd8
coderkhushal <137866759+coderkhushal@users.noreply.github.com>
2024-03-07 07:46:54 +0530

    changes

--- lib/screens/leaderboard_categories.dart
+++ lib/screens/leaderboard_categories.dart
@@ -12,37 +12,63 @@ class LeaderBoardCategories extends StatefulWidget {
 
 class _LeaderBoardCategoriesState extends State<LeaderBoardCategories> {
   List<Categories> categories = Categories.values;
+  List<Icon> icons= [
+    Icon(Icons.phone_android,color: Colors.white,size:40),
+    Icon(Icons.style,color: Colors.white,size:40),
+    Icon(Icons.local_grocery_store,color: Colors.white,size:40),
+    Icon(Icons.local_pharmacy,color: Colors.white,size:40),
+    Icon(Icons.toys,color: Colors.white,size:40),
+    Icon(Icons.sports,color: Colors.white,size:40),
+    Icon(Icons.book,color: Colors.white,size:40),
 
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: GridView.count(
-        crossAxisCount: 2, // Number of columns
-        children: List.generate(categories.length, (index) {
-          return InkWell(
-            onTap: () =>
-                context.pushNamed('leaderboard', extra: categories[index]),
-            child: Card(
-              elevation: 2,
-              margin: const EdgeInsets.all(7),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.teal,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Center(
-                  child: Text(
-                    categories[index].value,
-                    style: GoogleFonts.inter(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.yellow),
+    return Scaffold(
+      appBar: AppBar(
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.teal,
+        title: Center(
+          child: Text("Categories"),
+        ),
+      ),
+      body: Container(
+        child: GridView.count(
+          crossAxisCount: 2, // Number of columns
+          children: List.generate(categories.length, (index) {
+            return InkWell(
+              onTap: () =>
+                  context.pushNamed('leaderboard', extra: categories[index]),
+              child: Card(
+                elevation: 2,
+                margin: const EdgeInsets.all(7),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.teal.shade300,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+
+                      children: [
+                        icons[index],
+                        Text(
+
+                          categories[index].value,
+                          style: GoogleFonts.inter(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.grey.shade200),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          );
-        }),
+            );
+          }),
+        ),
       ),
     );
   }
